3171c87b4c9151cf828e75bbb1ef705cf751256b
zanjs <root@zanjs.com>
2015-10-08 09:24:13 +0800

    f

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require('gulp');
+
+var day = '20151008';
+
+// 引入组件
+var sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
+    minifycss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    browserify = require('gulp-browserify'),
+    notify = require('gulp-notify'),
+    autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload'),
+    port = process.env.port || 5000;
+
+
+
+
+// live reload
+gulp.task('connect',function(){
+    connect.server({
+        // root:'./',
+        port: port,
+        livereload: true,
+    })
+});
+
+
+//编译Sass，Autoprefix及缩小化
+gulp.task('sass', function() {
+    return gulp.src('./'+ day +'/src/scss/main.scss')
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('./'+ day +'/dist/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./'+ day +'/'))
+        .pipe( connect.reload() )
+        .pipe(notify({ message: 'Styles  task complete' }));
+
+});
+
+gulp.task('html',function(){
+    gulp.src('./'+ day +'/*.html')
+        .pipe( connect.reload() )
+});
+
+gulp.task('scripts',function(){
+    return gulp.src('./'+ day +'/src/js/pan.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./'+ day +'/dist/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('./'+ day +'/dist/js/'))
+        .pipe( connect.reload() )
+        .pipe(notify({ message: 'Scripts task complete' }));
+
+});
+
+/* 监听 文件变化  */
+
+gulp.task('watch', function() {
+
+    // 看守.scss 档
+    gulp.watch('./'+ day +'/src/scss/*.scss', ['sass']);
+
+    // 看守所有.js档
+    gulp.watch('./'+ day +'/src/js/*.js', ['scripts']);
+
+    // 看守所有.html
+    gulp.watch('./'+ day +'/*.html',['html']);
+
+});
+
+gulp.task('serve',['connect','watch']);
