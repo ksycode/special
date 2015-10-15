0286b4fd0d3e863771f3af6f93e8f2dfc7409f1c
zanjs <root@zanjs.com>
2015-10-15 18:11:46 +0800

    en

--- 20151015h5/src/js/d_gun.js
+++ 20151015h5/src/js/d_gun.js
@@ -1,57 +1,27 @@
-(function($){
-$.extend({
-        ZScroll:function(opt,callback){
-                //参数初始化
-                if(!opt) var opt={};
-                var _btnUp = $("#"+ opt.up);//Shawphy:向上按钮
-                var _btnDown = $("#"+ opt.down);//Shawphy:向下按钮
-                var timerID;
-                var _this=this.eq(0).find("ul:first");
-                var     lineH=_this.find("li:first").height(), //获取行高
-                        line=opt.line?parseInt(opt.line,10):parseInt(this.height()/lineH,10), //每次滚动的行数，默认为一屏，即父容器高度
-                        speed=opt.speed?parseInt(opt.speed,10):500; //卷动速度，数值越大，速度越慢（毫秒）
-                        timer=opt.timer //?parseInt(opt.timer,10):3000; //滚动的时间间隔（毫秒）
-                if(line==0) line=1;
-                var upHeight=0-line*lineH;
-                //滚动函数
-                var scrollUp=function(){
-                        _btnUp.unbind("click",scrollUp); //Shawphy:取消向上按钮的函数绑定
-                        _this.animate({
-                                marginTop:upHeight
-                        },speed,function(){
-                                for(i=1;i<=line;i++){
-                                        _this.find("li:first").appendTo(_this);
-                                }
-                                _this.css({marginTop:0});
-                                _btnUp.bind("click",scrollUp); //Shawphy:绑定向上按钮的点击事件
-                        });
+function ZScroll(obj,time){
 
-                }
-                //Shawphy:向下翻页函数
-                var scrollDown=function(){
-                        _btnDown.unbind("click",scrollDown);
-                        for(i=1;i<=line;i++){
-                                _this.find("li:last").show().prependTo(_this);
-                        }
-                        _this.css({marginTop:upHeight});
-                        _this.animate({
-                                marginTop:0
-                        },speed,function(){
-                                _btnDown.bind("click",scrollDown);
-                        });
-                }
-               //Shawphy:自动播放
-                var autoPlay = function(){
-                        if(timer)timerID = window.setInterval(scrollUp,timer);
-                };
-                var autoStop = function(){
-                        if(timer)window.clearInterval(timerID);
-                };
-                 //鼠标事件绑定
-                _this.hover(autoStop,autoPlay).mouseout();
-                _btnUp.css("cursor","pointer").click( scrollUp ).hover(autoStop,autoPlay);//Shawphy:向上向下鼠标事件绑定
-                _btnDown.css("cursor","pointer").click( scrollDown ).hover(autoStop,autoPlay);
+  var oMarquee = document.getElementById(obj);
+  var iLineHeight = 28;
+  var iLineCount = 6;
+  var iScrollAmount = 1;
 
-        }
-})
-})(Zepto);
+  function run() {
+  oMarquee.scrollTop += iScrollAmount;
+  if ( oMarquee.scrollTop == iLineCount * iLineHeight )
+  oMarquee.scrollTop = 0;
+  if ( oMarquee.scrollTop % iLineHeight == 0 ) {
+    setTimeout( function(){
+      run()
+    }, time );
+    } else {
+    setTimeout( function(){
+      run()
+    }, 50 );
+    }
+  }
+  oMarquee.innerHTML += oMarquee.innerHTML;
+  setTimeout( function(){
+    run()
+  }, time );
+
+}
--- 20151015h5/src/js/z_main.js
+++ 20151015h5/src/js/z_main.js
@@ -3,8 +3,6 @@ $(function(){
   	   new WOW().init();
 
 
-    //$("#z-list-hide").ZScroll({line:2,speed:1000,timer:3000,up:"but_up",down:"but_down"});
-
   };
 
 
